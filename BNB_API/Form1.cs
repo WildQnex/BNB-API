a4d2dce3b746f4060308f0bb08053bacbae1e9fb
WildQnex <konkord321@gmail.com>
2017-09-15 18:03:16 +0300

    Initial commit

---
+++ BNB_API/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.Net;
+using Newtonsoft.Json.Linq;
+
+namespace BNB_API
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string url = "http://www.nbrb.by/API/ExRates/Currencies";
+            string source = new WebClient().DownloadString(url);
+            dynamic obj = JArray.Parse(source);
+            string title;
+            foreach (dynamic el in obj)
+            {
+                title = "";
+                foreach (string tit in el)
+                {
+                    title = title + tit + "  |  ";
+                }
+                Box.Items.Add(title);
+            }
+        }
+    }
+}
---
+++ BNB_API/Program.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace BNB_API
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
+        }
+    }
+}
